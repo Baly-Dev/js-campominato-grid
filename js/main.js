044a23740fc4ba603e9ffeea39fbfc237812d428
baly <kevin@baldev.it>
2022-07-14 11:49:18 +0200

    Nubers append to cells

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
 // variables
 const grid = document.getElementById('grid')
-let cellEasy = 'cell-10'
-let cellHard = 'cell-9'
-let cellDifficult = 'cell-7'
-
-/* creare condizione per variabile width */
-let width = 10;
 let cells = []
+let cellClass;
+
+// creare condizione per variabile width
+let width = 10;
+const bombsNumber = 20
+let bombsArray = []
+
+if (width == 10){
+    cellClass = 'cell-10'
+}else if(width == 9){
+    cellClass = 'cell-9'
+}else{
+    cellClass = 'cell-7'
+}
 
 // function getGrid
 function getGrid(width){
+
+    // creo array per bombe e non
+    for (i=0; i < 20; i++){
+
+    }
+    
+
     // ciclo per cubo
     for (i=0; i < width * width; i++){
         // creo e aggiungo una cella nel document e nella lista cells 
         const cell = document.createElement('div')
         cell.setAttribute('id', i) 
-        cell.classList.add('cell-10')
+        cell.classList.add(cellClass)
+        if (i == 0){
+            cell.append(i + 1)
+        }else{
+            cell.append(i + 1)
+        }
         grid.append(cell)
         cells.push(cell)
     }
